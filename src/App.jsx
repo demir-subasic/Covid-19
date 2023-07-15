a9abd3c1d9ae9c1eca7deace598859b8282733c1
ensarvragic <vragicensar@gmail.com>
2023-07-16 00:35:24 +0200

    WorldSection and Footer(Icons)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './App.css'
-import WorldSection from './miniComponents/WorldSection'
-import Footer from './components/Footer/footer'
 import Home from './components/Home'
 import CovidStats from './components/CovidStats'
 import CountryStats from './components/CountryStats'
 import AboutUs from './components/AboutUs'
 import Navbar from './components/Home/navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import CoutryStats from './components/CountryStats'
+
 
 function App() {
 
