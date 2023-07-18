99bfa8429ac5ed23a788099e53358d697142a798
ensarvragic <vragicensar@gmail.com>
2023-07-18 18:03:21 +0200

    Added hovers effect and inserted routes in the WorldSection

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './navbar'
 import './index.scss'
+import WorldSection from '../../miniComponents/WorldSection'
+import Footer from '../Footer/footer'
+
 
 const Home = () => {
   return (
+    <>
     <div className='hero'>
       
     </div>
+    <WorldSection/>
+    <Footer/>
+    </>
   )
 }
 
